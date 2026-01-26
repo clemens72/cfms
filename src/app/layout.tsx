0e621b5459545d638b2b94dde06fb546e80a9f82
Eric Clemens <clemens72@gmail.com>
2026-01-26 12:25:02 -0500

    added meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat, Arimo } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 import NavBar from "@/components/layout/NavBar";
@@ -30,9 +31,48 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const metaPixelId = 991364196395827;
+
   return (
     <html lang="en" className={`${arimo.variable} ${montserrat.variable}`}>
       <body>
+        {metaPixelId && (
+          <>
+            <Script
+              id="meta-pixel"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  if (!window.fbq) {
+                    window.fbq = function() {
+                      window.fbq.callMethod ? window.fbq.callMethod.apply(window.fbq, arguments) : window.fbq.queue.push(arguments);
+                    };
+                    if (!window._fbq) window._fbq = window.fbq;
+                    window.fbq.push = window.fbq;
+                    window.fbq.loaded = true;
+                    window.fbq.version = '2.0';
+                    window.fbq.queue = [];
+                    const t = document.createElement('script');
+                    t.async = true;
+                    t.src = 'https://connect.facebook.net/en_US/fbevents.js';
+                    const s = document.getElementsByTagName('script')[0];
+                    s.parentNode.insertBefore(t, s);
+                  }
+                  fbq('init', ${JSON.stringify(metaPixelId)});
+                  fbq('track', 'PageView');
+                `,
+              }}
+            />
+            <noscript>
+              <img
+                height="1"
+                width="1"
+                style={{ display: 'none' }}
+                src={`https://www.facebook.com/tr?id=${metaPixelId}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+          </>
+        )}
         <NavBar />
         <div className="pt-16">
           {children}
